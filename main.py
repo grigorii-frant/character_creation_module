4318e8325881a8c9b063be28dbc29e26919e65d5
Григорий Костыгов <grikos123@gmail.com>
2023-11-06 17:05:28 +0300

    добалены докстриеги

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Функция атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +16,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -25,6 +27,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция специальных умений."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -36,6 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция начала тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -47,7 +51,7 @@ def start_training(char_name: str, char_class: str) -> str:
           'defence — чтобы блокировать атаку противника или '
           'special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd: str = None
+    cmd: str = ''
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -60,6 +64,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора класса."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -82,6 +87,7 @@ def choice_char_class() -> str:
 
 
 def main():
+    """Функция main."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
---
+++ tmp.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+
+message = f'Добро пожаловать в самую лучшую программу для вычисления '
+          f'квадратного корня из заданного числа'
+print (message)
+
+
+def  CalculateSquareRoot (number):
+    """ Вычисляет квадратный корень"""
+    return  sqrt(number)
+
+
+def calc(your_number) :
+    if your_number<=0:
+        return    
+     
+    root = 0
+    print(f'Мы вычислили квадратный корень из введённого вами числа. '
+          f'Это будет: {CalculateSquareRoot(your_number)}')
+
+
+print(message)
+calc (25.5)
